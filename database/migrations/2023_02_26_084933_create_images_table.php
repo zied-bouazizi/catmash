e9d5df019ca931fa57a1d5966c8b2deb1ccb7112
Zied Bouazizi <zied.bouazizi@outlook.com>
2023-02-26 09:54:01 +0100

    Create image model & migration

---
+++ database/migrations/2023_02_26_084933_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->integer('score')->unsigned()->default(1500);
+            $table->integer('wins')->unsigned()->default(0);
+            $table->integer('losses')->unsigned()->default(0);
+            $table->integer('rank')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+};
